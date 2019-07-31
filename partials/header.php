21d8f16d3de6e64db254afdccfc782ca5d422c88
Armandot1998 <armandotcajilema@gmail.com>
2019-07-30 20:30:11 -0500

    Login y creación de interfaces iniciales

---
+++ partials/header.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="utf-8">
+    <link href="https://fonts.googleapis.com/css?family=Roboto" rel="stylesheet">
+    <link rel="stylesheet" href="assets/css/style.css">
+  </head>
+<header>
+<div class="container-fluid" style="background-color: #d02184;" >
+	<div class="row">
+		<div class="col-md-12">
+		</div>
+	</div>
+</div>
+</header>
+</html>
+
+
---
+++ script/CamilaSalome.sql
@@ -0,0 +1,7 @@
+create database CamilaSalome;
+use CamilaSalome;
+
+create table users (
+id int auto_increment primary key,
+email varchar(100) null,
+password varchar(100));
