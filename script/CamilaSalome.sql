3cc5a9766506148de77820a01dae819a1f6bd18a
Rodney Jacome <rjacome@sidesoft.com.ec>
2019-08-04 22:17:45 -0500

    Querys (subida de prueba)

--- script/CamilaSalome.sql
+++ script/CamilaSalome.sql
@@ -5,3 +5,22 @@ create table users (
 id int auto_increment primary key,
 email varchar(100) null,
 password varchar(100));
+
+create table users (
+id int auto_increment primary key,
+email varchar(100) null,
+password varchar(100));
+
+create table image(
+id_image int auto_increment primary key,
+name varchar(250) not null);
+
+create table blog(
+id_blog int auto_increment primary key,
+title varchar(250) not null,
+author varchar(250) null,
+date_blog timestamp not null,
+id_image int not null,
+content text not null
+FOREIGN KEY (id_image) REFERENCES image(id_image)
+);
