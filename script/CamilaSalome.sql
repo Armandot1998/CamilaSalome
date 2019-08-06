aa96e19ba8d57b26fe97d0ff14765c6b8413bb44
wen <jake_j98@hotmail.com>
2019-08-06 14:32:12 -0500

    script actualizado

--- script/CamilaSalome.sql
+++ script/CamilaSalome.sql
@@ -6,10 +6,14 @@ id int auto_increment primary key,
 email varchar(100) null,
 password varchar(100));
 
-create table users (
-id int auto_increment primary key,
-email varchar(100) null,
-password varchar(100));
+create table gallery(
+id_gallery int auto_increment primary key,
+category varchar(255) not null,
+id_image int not null,
+title varchar(255) not null,
+status tinyint(1) NOT NULL DEFAULT '1' COMMENT '1=Active, 0=Inactive',
+FOREIGN KEY (id_image) REFERENCES image(id_image)
+);
 
 create table image(
 id_image int auto_increment primary key,
@@ -21,6 +25,6 @@ title varchar(250) not null,
 author varchar(250) null,
 date_blog timestamp not null,
 id_image int not null,
-content text not null
+content text not null,
 FOREIGN KEY (id_image) REFERENCES image(id_image)
 );
